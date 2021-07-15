e2e5f9bf60542d3e0429d706297301c514a617a6
Rishit Gupta <rishitgupta2013@gmail.com>
2021-07-15 14:32:42 +0530

    Keeper App

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightIcon from "@material-ui/icons/Highlight";
+
+function Header() {
+  return (
+    <header>
+       <img src="https://i.postimg.cc/KjVLqHbr/mkbhd-logo-4k-hd-wallpaper-thumb-removebg-preview.png" alt="" style={{width: "10%"}}/>
+      <h1>
+        MKBHD Keeper App
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
